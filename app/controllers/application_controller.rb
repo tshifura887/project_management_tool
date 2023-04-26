67465a6bccb61386ee9b5f42508ff20e18368f33
tshifura887 <85707650+tshifura887@users.noreply.github.com>
2023-04-26 09:47:55 +0200

    feat: create project

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to root_url, alert: exception.message
+  end
+  
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :role])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :role])
+  end  
+
+  def current_ability
+    @current_ability ||= Ability.new(current_user)
+  end
 end
