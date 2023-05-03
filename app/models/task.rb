4404d3981a231cc346e313de5efd361571a42133
tshifura887 <85707650+tshifura887@users.noreply.github.com>
2023-05-03 11:46:04 +0200

    feat: add tasks to projects

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,21 @@
 class Task < ApplicationRecord
   belongs_to :project
-  belongs_to :user
+  belongs_to :assignee, class_name: 'User', foreign_key: 'user_id', optional: true
+
+  has_many :attachments, dependent: :destroy
+  accepts_nested_attributes_for :attachments, allow_destroy: true
+
+  has_many :comments
+  
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :days, presence: true
+  validates :priority, presence: true
+  validates :status, presence: true
+
+  def send_task_updated_notification
+    project_owner_email = project.user.email
+    project_owner_name = project.user.name
+    ProjectMailer.task_updated(self, project_owner_email, project_owner_name).deliver_now
+  end
 end
